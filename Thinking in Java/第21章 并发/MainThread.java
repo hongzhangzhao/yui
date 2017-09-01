32168b5b292d4af6743548864a3c9e358227050c
zhaohongzhang <hongzhangzhao@outlook.com>
2017-09-01 22:30:52 +0800

    concurrent

---
+++ Thinking in Java/第21章 并发/MainThread.java
@@ -0,0 +1,9 @@
+package com.MyJavaBase.concurrency;
+
+public class MainThread {
+    public static void main(String[] args) {
+        LiftOff launch = new LiftOff();
+        launch.run();
+    }
+
+}
