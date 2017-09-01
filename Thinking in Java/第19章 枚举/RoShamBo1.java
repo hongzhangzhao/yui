93e9ca4e1114dd446c530986d7212025ae37587d
zhaohongzhang <hongzhangzhao@outlook.com>
2017-09-01 22:39:38 +0800

    a

--- Thinking in Java/第19章 枚举/RoShamBo1.java
+++ Thinking in Java/第19章 枚举/RoShamBo1.java
@@ -1,4 +1,4 @@
-package com.MyJavaBase.Enums;
+package com.MyJavaBase.Enumerated;
 
 /**
  * 19.11 多路分发
@@ -6,10 +6,10 @@ package com.MyJavaBase.Enums;
  * */
 
 import java.util.*;
-import static com.MyJavaBase.Enums.Outcome.*;
+import static com.MyJavaBase.Enumerated.Outcome.*;
 
 
-interface Item {
+interface Item{
     Outcome compete(Item it);
     Outcome eval(Paper p);
     Outcome eval(Scissors s);
@@ -17,50 +17,51 @@ interface Item {
 }
 
 class Paper implements Item {
-    public Outcome compete(Item it){ return it.eval(this); }
-    public Outcome eval(Paper p ) { return DRAW; }
+    public Outcome compete (Item it) { return it.eval(this); }
+    public Outcome eval(Paper p) { return DRAW; }
     public Outcome eval(Scissors s) { return WIN; }
     public Outcome eval(Rock r) { return LOSE; }
-    public String toString () { return "Paper";}
+    public String toString() { return "Paper"; }
 }
 
 class Scissors implements Item {
-    public Outcome compete (Item it) { return it.eval(this); }
-    public Outcome eval (Paper p) { return LOSE; }
-    public Outcome eval (Scissors s) { return DRAW;}
-    public Outcome eval (Rock r) {return WIN; }
-    public String toString(){ return "Scissors"; }
+    public Outcome compete(Item it){ return it.eval(this); }
+    public Outcome eval(Paper p) { return LOSE; }
+    public Outcome eval(Scissors s){ return DRAW;}
+    public Outcome eval(Rock r) { return WIN; }
+    public String toString(){
+        return "Scissors";
+    }
 }
 
 class Rock implements Item {
-    public Outcome compete(Item it){ return it.eval(this); }
-    public Outcome eval(Paper p) { return WIN; }
-    public Outcome eval (Scissors s) { return LOSE; }
-    public Outcome eval (Rock r) { return WIN; }
-    public String toString() { return "Scissors"; }
-}
+    public Outcome compete(Item it){ return it.eval(this);}
+    public Outcome eval(Paper p){ return WIN; }
+    public Outcome eval(Scissors s) { return LOSE; }
+    public Outcome eval(Rock r){ return DRAW; }
+    public String toString(){
+        return "Rock";
+    }
 
+}
 
 public class RoShamBo1 {
     static final int SIZE = 20;
     private static Random rand = new Random(47);
-    public static Item newItem(){
+    public static Item newItem(){  //随机返回 Item的实现对象
         switch(rand.nextInt(3)){
             default:
-            case 0:return new Scissors();
-            case 1:return new Paper();
-            case 2:return new Rock();
+            case 0 : return new Scissors();
+            case 1 : return new Paper();
+            case 2 : return new Rock();
         }
     }
-    public static void match(Item a, Item b) {
-        System.out.println(
-                a + " vs. " + b + ": " + a.compete(b)
-        );
+    public static void match(Item a,Item b) {
+        System.out.println(a + " vs. " + b + ": " + a.compete(b));
     }
 
-    public static void main (String[] args){
+    public static void main(String[] args) {
         for(int i=0;i<SIZE;i++)
-            match(newItem(), newItem());
+            match(newItem(),newItem());
     }
-
 }
