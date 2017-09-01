842d04afdcbce8f60f0f22504a44b9995a4d5af6
zhaohongzhang <hongzhangzhao@outlook.com>
2017-09-01 22:41:17 +0800

    an

---
+++ Thinking in Java/第20章 注解/Testable.java
@@ -0,0 +1,13 @@
+package com.MyJavaBase.Annotations;
+
+public class Testable {
+
+    public void execute(){
+        System.out.println("Executing..");
+
+    }
+
+    // @Test void testExecute(){ execute(); }
+
+
+}
